cbe660baf9d7cbbe0334259ace8b2e79cba31e38
DikanR <dikan42724@gmail.com>
2023-03-30 15:59:49 +0800

    added updateDTD() and saveAs()

--- fixing2.php
+++ fixing2.php
@@ -1,5 +1,6 @@
 <?php
-    function importFile($file){
+    function importFile($file)
+    {
         $doc = new DOMDocument();
         $doc->formatOutput = true;
         $doc->preserveWhiteSpace = false;
@@ -8,44 +9,97 @@
         return $doc;
     }
 
-    function duplicationFix($imported){
+    function saveAs($imported, $savedName): void
+    {
+        $imported->save($savedName);
+    
+        // No Break line fixes
+        $imported = importFile($savedName);
+        $imported->save($savedName);
+    
+        echo "Saved as \"{$savedName}\"\n";
+    }
+
+    function updateDTD($imported)
+    {
+        $importedRoot = $imported->documentElement;
+        $imp = new DOMImplementation;
+        $dtd = $imp->createDocumentType(
+            'article',
+            '-//NLM//DTD JATS (Z39.96) Journal Publishing DTD with MathML3 v1.3 20210610//EN',
+            'JATS-journalpublishing1-3-mathml3.dtd'
+        );
+        $imported = $imp->createDocument("", "", $dtd);
+        $imported->encoding = 'UTF-8';
+        $node = $imported->importNode($importedRoot, true);
+        $imported->appendChild($node);
+
+        echo "DTD updated to 1.3";
+
+        return $imported;
+    }
+
+    function incrementalDuplicationFix($imported, $elementName, $attributeName)
+    {
+        $intArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
         $importedRoot = $imported->documentElement;
         $increment = 0;
-        foreach ($importedRoot->getElementsByTagName('sup') as $supTag) {
-            if (str_contains($supTag->getAttribute('id'), '_')) {
-                $stripPosition = strpos($supTag->getAttribute('id'), '-');
-        
-                $string = substr($supTag->getAttribute('id'), 0, $stripPosition);
-                $increment++;
-        
-                $setAttribute = "{$string}-{$increment}";
-        
-                $supTag->setAttribute('id', $setAttribute);
+
+        // Look for integer in attribute value to find separation position between string and integer
+        foreach ($importedRoot->getElementsByTagName($elementName)->item(0)->getAttribute($attributeName) as $str) {
+            if (in_array($str, $intArray)) {
+                $separatorPosition = strpos($importedRoot->getElementsByTagName($elementName)->item(0)->getAttribute($attributeName), $str);
             }
         }
 
-        return "Duplication Fixed";
+        foreach ($importedRoot->getElementsByTagName($elementName) as $elementTag) {
+            $string = substr($elementTag->getAttribute($attributeName), 0, $separatorPosition);
+            $increment++;
+    
+            $setAttribute = "{$string}{$increment}";
+    
+            $elementTag->setAttribute($attributeName, $setAttribute);
+        }
+
+        return "<{$elementName}> Duplication Fixed";
     }
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------//
     
     echo "Welcome to JATS fixer\n";
-    $fileName = readline("Masukan nama file: \n");
-    importFile($fileName);
+    $fileName = readline("Enter filename: ");
+    $imported = importFile($fileName);
     
     while (true) {
         echo <<<INFO
+        --------------------------------
         Available option
-        1.
-        2.
+        1. Incremental duplication fix
+        2. Update DTD to 1.3
+        3. saveAs
+        --------------------------------
 
         INFO;
-        $function = readline("Select option: \n");
+        $function = readline("Select option: ");
         switch ($function) {
             case '1':
-                echo "Dayum\n";
+                $elementName = readline("Element name: ");
+                $attributeName = readline("Attribute name: ");
+                incrementalDuplicationFix($imported, $elementName, $attributeName);
+                break;
+
+            case '2':
+                $imported = updateDTD($imported);
+                break;
+
+            case '3':
+                $savedName = readline("Save as: ");
+                saveAs($imported, $savedName);
+                exit;
                 break;
             
             default:
-                # code...
+                echo "Please select available numbers\n";
                 break;
         }
     }

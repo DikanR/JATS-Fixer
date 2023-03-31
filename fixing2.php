1d7b88f0e956ef63ad05461fc6eb23bdcb615179
DikanR <dikan42724@gmail.com>
2023-03-31 15:30:08 +0800

    added addOrReplaceElementAttribute()

--- fixing2.php
+++ fixing2.php
@@ -20,6 +20,118 @@
         echo "Saved as \"{$savedName}\"\n";
     }
 
+    function addOrReplaceElementAttribute($imported, $elementName, $includeNamespace, $attributeName, $attributeValue, $runType, ?string $designatedValue)
+    {
+        // Note: Could have numerous elements not only one
+        $importedRoot = $imported->documentElement;
+
+        switch ($runType) {
+            // Replace all corresponding elements
+            case '1':
+                // Check if its include a namespace
+                if (strtoupper($includeNamespace) == 'Y') {
+                    // check if its root element
+                    if ($elementName == 'article') {
+                        $importedRoot->setAttributeNS(
+                            'http://www.w3.org/2000/xmlns/',
+                            "xmlns:{$attributeName}",
+                            $attributeValue
+                        );
+                    } else {
+                        foreach ($importedRoot->getElementsByTagName($elementName) as $elementTag) {
+                            $elementTag->setAttributeNS(
+                                'http://www.w3.org/2000/xmlns/',
+                                "xmlns:{$attributeName}",
+                                $attributeValue
+                            );
+                        }
+                    }
+                } else {
+                    if ($elementName == 'article') {
+                        $importedRoot->setAttribute($attributeName, $attributeValue);
+                    } else {
+                        foreach ($importedRoot->getElementsByTagName($elementName) as $elementTag) {
+                            $elementTag->setAttribute($attributeName, $attributeValue);
+                        }
+                    }
+                }
+                break;
+            
+            // Replace only the same attribute name
+            case '2':
+                // Check if its include a namespace
+                if (strtoupper($includeNamespace) == 'Y') {
+                    // check if its root element
+                    if ($elementName == 'article') {
+                        $importedRoot->setAttributeNS(
+                            'http://www.w3.org/2000/xmlns/',
+                            "xmlns:{$attributeName}",
+                            $attributeValue
+                        );
+                    } else {
+                        foreach ($importedRoot->getElementsByTagName($elementName) as $elementTag) {
+                            if ($elementTag->hasAttribute($attributeName)) {
+                                $elementTag->setAttributeNS(
+                                    'http://www.w3.org/2000/xmlns/',
+                                    "xmlns:{$attributeName}",
+                                    $attributeValue
+                                );
+                            }
+                        }
+                    }
+                } else {
+                    if ($elementName == 'article') {
+                        $importedRoot->setAttribute($attributeName, $attributeValue);
+                    } else {
+                        foreach ($importedRoot->getElementsByTagName($elementName) as $elementTag) {
+                            if ($elementTag->hasAttribute($attributeName)) {
+                                $elementTag->setAttribute($attributeName, $attributeValue);
+                            }
+                        }
+                    }
+                }
+                break;
+
+            case '3':
+                // Check if its include a namespace
+                if (strtoupper($includeNamespace) == 'Y') {
+                    // check if its root element
+                    if ($elementName == 'article') {
+                        $importedRoot->setAttributeNS(
+                            'http://www.w3.org/2000/xmlns/',
+                            "xmlns:{$attributeName}",
+                            $attributeValue
+                        );
+                    } else {
+                        foreach ($importedRoot->getElementsByTagName($elementName) as $elementTag) {
+                            if ($elementTag->hasAttribute($attributeName) && $elementTag->getAttribute($attributeName) == $designatedValue) {
+                                $elementTag->setAttributeNS(
+                                    'http://www.w3.org/2000/xmlns/',
+                                    "xmlns:{$attributeName}",
+                                    $attributeValue
+                                );
+                            }
+                        }
+                    }
+                } else {
+                    if ($elementName == 'article') {
+                        $importedRoot->setAttribute($attributeName, $attributeValue);
+                    } else {
+                        foreach ($importedRoot->getElementsByTagName($elementName) as $elementTag) {
+                            if ($elementTag->hasAttribute($attributeName) && $elementTag->getAttribute($attributeName) == $designatedValue) {
+                                $elementTag->setAttribute($attributeName, $attributeValue);
+                            }
+                        }
+                    }
+                }
+                break;
+            
+            default:
+                # code...
+                break;
+        }
+    }
+
     function updateDTD($imported)
     {
         $importedRoot = $imported->documentElement;
@@ -77,50 +189,68 @@
     }
 
 // ----------------------------------------------------------------------------------------------------------------------------------------------//
-    
+    // Prompt starts here
     echo "Welcome to JATS fixer\n";
-    $fileName = readline("Enter filename: ");
-    $imported = importFile($fileName);
+    while (true) {
+        $fileName = readline("Masukan nama file: ");
+        $imported = importFile($fileName);
+        if (error_get_last()) {
+            echo "\033[031mNama file tidak valid\nMohon masukan nama file yang benar beserta ekstensinya (.xml)\033[0m\n";
+            error_clear_last();
+        } else {
+            break;
+        }
+    }
     
     while (true) {
         echo <<<INFO
-        --------------------------------
-        Available option
-        1. Increment attribute value
-        2. Update DTD to 1.3
-        3. Add or replace element attribute
-        4. Save as
-        --------------------------------
-
+        ---------------------------------------
+        Opsi tersedia
+        1. Perbaiki increment nilai attribute
+        2. Update DTD ke 1.3
+        3. (WIP)
+        4. Tambah atau ganti attribute element
+           beserta Nilainya
+        5. Save as
+        ---------------------------------------
+        \n
         INFO;
         $function = readline("Select option: ");
         switch ($function) {
             case '1':
                 echo <<<INFO
-                ----------------------------------------
-                This option fix the incremental problem;
-                - Number duplication
-                - Assign incremental number to existing
-                  attributes
-                ----------------------------------------
-        
+                -----------------------------------------
+                Opsi ini memperbaiki masalah incremental
+                \033[33m(contoh-1, contoh-2, contoh-3)\033[0m pada nilai
+                attribute, seperti;
+
+                - Duplikasi angka pada nilai attribute
+                  \033[33m(contoh-1, contoh-1, contoh-2)\033[0m
+                - Menambahkan angka pada nilai atribute
+                  yang terduplikasi dan tanpa angka
+                  \033[33m(contoh, contoh, contoh)\033[0m
+                -----------------------------------------
+                \n
                 INFO;
+                echo "<\033[32melement-name\033[0m attribute-name=\"attribute-value\">\n";
                 $elementName = readline("Element name: ");
+                echo "<{$elementName} \033[32mattribute-name \033[0m=\"\">\n";
                 $attributeName = readline("Attribute name: ");
                 echo <<<INFO
                 ----------------------------------------
-                You can enter the attribute value
-                with or without the number, example;
-                - value-6
-                - value6
-                - value-
-                - value
-                to assign incremental to non-number
-                attribute please do not include number
-                since it has no number.
+                nilai attribute yang dimasukan dapat
+                beserta maupun tanpa angka, contoh;
+                - contoh-6
+                - contoh6
+                - contoh-
+                - contoh
+                to assign incremental to duplicated
+                non-number attribute please do not
+                include number since it has no number.
                 ----------------------------------------
-        
+                \n
                 INFO;
+                echo "<{$elementName} {$attributeName}=\"\033[32mattribute-value\033[0m\">\n";
                 $attributeValue = readline("Attribute value: ");
                 incrementalAttributeFix($imported, $elementName, $attributeName, $attributeValue);
                 break;
@@ -128,12 +258,48 @@
             case '2':
                 $imported = updateDTD($imported);
                 break;
-
+            
+            
             case '3':
-                ;
+                $imported = updateDTD($imported);
+                break;
+            
+            // Add or replace attribute
+            case '4':
+                echo <<<INFO
+
+                1. Timpa semua element yang sama
+                2. Timpa hanya nama attribute yang sama
+                3. Timpa hanya nilai attribute yang ditentukan
+                \n
+                INFO;
+                $runType = readline("Tipe running: ");
+                if ($runType == 3) {
+                    $designatedValue = readline("Nilai attribute yang akan ditimpa: ");
+                }
+                $elementName = readline("Nama Element: ");
+                $attributeName = readline("Nama attribute: ");
+                $attributeValue = readline("Nilai attribute yang baru: ");
+                echo <<<INFO
+
+                gunakan namespace \033[32mxmlns\033[0m?
+                \033[32mxmlns\033[0m:attribute="nilai-attribute"
+                Y/N
+                \n
+                INFO;
+                $includeNamespace = readline("Include: ");
+                addOrReplaceElementAttribute(
+                    $imported,
+                    $elementName,
+                    $includeNamespace,
+                    $attributeName, 
+                    $attributeValue,
+                    $runType,
+                    $designatedValue
+                );
                 break;
 
-            case '4':
+            case '5':
                 $savedName = readline("Save as: ");
                 saveAs($imported, $savedName);
                 exit;
